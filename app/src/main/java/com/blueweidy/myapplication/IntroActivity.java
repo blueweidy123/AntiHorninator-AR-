2f21d81e3059d51cefb4e229ead5226b89ad1c17
bluewiedy123 <anlalahello@gmail.com>
2021-08-06 15:53:39 +0700

    WELCOME SCREEN ADDED

--- app/src/main/java/com/blueweidy/myapplication/IntroActivity.java
+++ app/src/main/java/com/blueweidy/myapplication/IntroActivity.java
@@ -3,7 +3,15 @@ package com.blueweidy.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -15,14 +23,30 @@ public class IntroActivity extends AppCompatActivity {
     private ViewPager screenPager;
     IntroViewPagerAdapter introViewPagerAdapter;
     TabLayout tabIndicator;
+    Button btnNext;
+    Button btnGetStarted;
+    Animation btnAnim;
+    int position = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        if (restorePrefData()){
+            Intent startAtiv = new Intent(getApplicationContext(), startActiv.class);
+            startActivity(startAtiv);
+            finish();
+        }
+
         setContentView(R.layout.activity_intro);
 
+        btnNext = findViewById(R.id.btn_next);
+        btnGetStarted = findViewById(R.id.btn_get_started);
         tabIndicator = findViewById(R.id.tab_indicator);
-
+        btnAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.button_animation);
 
         List<ScreenItem> mList = new ArrayList<>();
         mList.add(new ScreenItem("TKB", "mot thu gi do rat dai dong de test layout cua cai textview nay va no can dai hon nua van chua du .-. 1 ti nua", R.drawable.intro_tkb));
@@ -34,5 +58,78 @@ public class IntroActivity extends AppCompatActivity {
         screenPager.setAdapter(introViewPagerAdapter);
 
         tabIndicator.setupWithViewPager(screenPager);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                position = screenPager.getCurrentItem();
+                if (position < mList.size()){
+                    position++;
+                    screenPager.setCurrentItem(position);
+                }
+                if (position == mList.size()-1){
+                    loadLastScreen();
+                }
+            }
+        });
+
+        tabIndicator.addOnTabSelectedListener(new TabLayout.BaseOnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+                if (tab.getPosition() == mList.size()-1){
+                    loadLastScreen();
+                }
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        btnGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent startAtiv = new Intent(getApplicationContext(), startActiv.class);
+                startActivity(startAtiv);
+                savePrefsData();
+                finish();
+
+            }
+        });
+
+    }
+
+    private boolean restorePrefData() {
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("myprefs", MODE_PRIVATE);
+        boolean isIntroOpenedB4 = pref.getBoolean("isIntroOpened", false);
+        return isIntroOpenedB4;
+
+    }
+
+    private void savePrefsData(){
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("myprefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putBoolean("isIntroOpened", true);
+        editor.commit();
+    }
+
+    private void loadLastScreen(){
+
+        btnNext.setVisibility(View.INVISIBLE);
+        btnGetStarted.setVisibility(View.VISIBLE);
+        tabIndicator.setVisibility(View.INVISIBLE);
+
+        btnGetStarted.setAnimation(btnAnim);
+
     }
 }
